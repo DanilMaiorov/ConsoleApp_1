4266d7d9d24070b43670392fcc7bda2bcbf2fd37
Danil Uktus <damayorov93@gmail.com>
2025-01-25 23:29:05 +0500

    DZ_1 added

---
+++ DZ_1_SimpleCode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DZ_1_SimpleCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
